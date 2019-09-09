9657c8ba49c82c78d1e6e0fa94c4eb2989771a1e
LouisG99 <louisgrd@umich.edu>
2019-09-09 14:33:13 -0400

    lab 0 task

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+import random
+
+print('Louis')
+randA = random.randint(0, 100)
+randB = random.randint(0, 100)
+print(randA)
+print(randB)
+
+print('Sum =', randA+randB)
+print('Average =', (randA+randB)/2)
+
